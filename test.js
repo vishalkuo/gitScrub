1448577c53a72dcc208be2e80fe4c1a37b4591b2
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-07-15 06:50:44 -0400

    scrubbadubdub

--- test.js
+++ test.js
@@ -2,8 +2,14 @@ var gitscrub = require('./lib/gitscrub');
 var secret = require('./lib/secret');
 
 value = undefined;
-
 gitscrub.authenticate(secret.username, secret.password, function(resultData){
     value = resultData;
-    console.log(value);
+    //console.log(value);
+    gitscrub.getAllRepos(function(repoData){
+//        console.log(JSON.stringify(resultData[0]['id']))
+        for (var result in repoData){
+            console.log(repoData[result]['id'])
+        }
+    })
 });
+

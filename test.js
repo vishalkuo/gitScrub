1666f2eaf77b8c6f533e5b2d0a105e4c34088dac
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-07-15 18:41:08 -0400

    mainLogic is in test js lol

--- test.js
+++ test.js
@@ -5,22 +5,33 @@ value = undefined;
 
 
 
-var promise = new RSVP.Promise(function(resolve, reject){
-    gitscrub.authenticate(secret.username, secret.password, function(resultData){
-    value = resultData;
-    if (value != null){
-        console.log("here")
-        resolve(value)
-    }else{
-        reject(Error("It Broke"))
-    }
+var promise = new RSVP.Promise(function(resolve, reject) {
+    gitscrub.authenticate(secret.username, secret.password, function(resultData) {
+        value = resultData;
+        if (value != null) {
+            resolve(value)
+        } else {
+            reject(Error("It Broke"))
+        }
     });
 })
 
-promise.then(function(result){
-    gitscrub.getAllRepos(function(repoData){
-        for (var result in repoData){
-            console.log(repoData[result]['id'])
+promise.then(function(result) {
+    gitscrub.getAllRepos(function(repoData) {
+        objArr = new Array();
+        for (var result in repoData) {
+            objArr.push(repoData[result]['name'])
         }
-    })
+        /*for (var i in objArr){
+            gitscrub.grabReadMes(objArr[i], function(result){
+                var b64 string = result[0]['content']
+
+            })   
+        }*/
+        gitscrub.grabReadMes(objArr[0], function(result){
+            var b64string = result['content']  
+            var buf = new Buffer(b64string, 'base64')
+            console.log(buf.toString('utf-8'))
+        })
+    });
 })

73b9b242d8527e082dd3b655e705c2377b1ef523
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-07-19 19:50:18 -0400

    Parse all readmes

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-var gitscrub = require('./lib/gitscrub');
-var secret = require('./lib/secret');
-var RSVP = require('rsvp');
-value = undefined;
+var gitscrub = require('./lib/gitscrub')
+var secret = require('./lib/secret')
+var RSVP = require('rsvp')
+value = undefined
+objArr = []
 
 
 
-var promise = new RSVP.Promise(function(resolve, reject) {
+promise = new RSVP.Promise(function(resolve, reject) {
     gitscrub.authenticate(secret.username, secret.password, function(resultData) {
         value = resultData;
-        if (value != undefined) {
+        if (value !== undefined) {
             resolve(value)
         } else {
             reject(Error("It Broke"))
@@ -17,24 +18,34 @@ var promise = new RSVP.Promise(function(resolve, reject) {
 })
 
 promise.then(function(result) {
-    gitscrub.getAllRepos(function(repoData) {
-        objArr = new Array();
-        for (var result in repoData) {
-            objArr.push(repoData[result]['name'])
-        }
-        // gitscrub.grabAllReadmes(null, null, function(res) {
-        //     gitscrub.parseAllReadMes(objArr, null, function(data) {
+     nestedPromise = new RSVP.Promise(function(resolve, reject) {
+        gitscrub.getAllRepos(function(repoData) {
+                for (var result in repoData) {
+                    objArr.push(repoData[result].name)
+                }
+                resolve(objArr)
+
+            })
+            // gitscrub.grabAllReadmes(function(data) {
+            //     console.log(data)
+            // })
+            // gitscrub.grabReadMeAtRepo(objArr[6], function(result){
+            //     
+            // })
+
+    })
+    nestedPromise.then(function(data0) {
+        // gitscrub.grabReadMeAtRepo(data[8], function(result) {
+        //     var b64string = result.content
+        //     var buf = new Buffer(b64string, 'base64')
+        //     gitscrub.parseReadMe(buf.toString('utf-8'), null, function(data) {
         //         console.log(data)
         //     })
-        //})
-
-        gitscrub.grabReadMeAtRepo(objArr[8], function(result){
-            var b64string = result['content']
-            var buf = new Buffer(b64string, 'base64')
-            gitscrub.parseReadMe(buf.toString('utf-8'), null, function(data){
-                console.log(data)
+        // })
+        gitscrub.grabAllReadmes(function(data1){
+            gitscrub.parseAllReadMes(null, function(data2){
+                console.log(data2)
             })
         })
-
     })
 })
